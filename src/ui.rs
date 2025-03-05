9baff65c64d976704a0ba8b381dee19ddd92f464
animeshchaudri <animeshchaudhri3@gmail.com>
2025-03-05 14:24:46 +0530

    Wow its done

--- src/ui.rs
+++ src/ui.rs
@@ -84,8 +84,7 @@ pub fn start(args: &mut [String]) {
     let mut frame = sciter::WindowBuilder::main_window().create();
     #[cfg(windows)]
     allow_err!(sciter::set_options(sciter::RuntimeOptions::UxTheming(true)));
-    frame.set_title(&crate::get_app_name());
-    #[cfg(target_os = "macos")]
+    frame.set_title("Chandan's Desk");    #[cfg(target_os = "macos")]
     crate::platform::delegate::make_menubar(frame.get_host(), args.is_empty());
     #[cfg(windows)]
     crate::platform::try_set_window_foreground(frame.get_hwnd() as _);
