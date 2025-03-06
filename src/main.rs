a6f12ff423f9ad4b9fb57f8a13d5782e8e2d1f2d
animeshchaudri <animeshchaudhri3@gmail.com>
2025-03-06 07:31:29 +0530

    Feat: chandan ki chandni

--- src/main.rs
+++ src/main.rs
@@ -48,10 +48,10 @@ fn main() {
         -k, --key=[KEY] ''
        -s, --server=[] 'Start server'",
     );
-    let matches = App::new("rustdesk")
+    let matches = App::new("ChandanDesk")
         .version(crate::VERSION)
-        .author("Purslane Ltd<info@rustdesk.com>")
-        .about("RustDesk command line tool")
+        .author("Chadan Ltd<info@chandan.com>")
+        .about("Chandan command line tool")
         .args_from_usage(&args)
         .get_matches();
     use hbb_common::{config::LocalConfig, env_logger::*};
